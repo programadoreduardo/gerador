11700f46c0d6834364000083df620c685191a6d8
programadoreduardo <programador.eduardo@gmail.com>
2024-09-04 20:33:47 -0300

    modal terminado

--- App.js
+++ App.js
@@ -46,7 +46,7 @@ export default function App() {
       </TouchableOpacity>
 
       <Modal visible={modalVisible} animationType="fade" transparent = {true}>
-        <ModalPassword/>
+        <ModalPassword password={passwordValue} handleClose={()=> setModalVisible(false)}/>
       </Modal>
 
     </View>
--- src/modal/index.js
+++ src/modal/index.js
@@ -1,14 +1,25 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-export function ModalPassword() {
+export function ModalPassword({password, handleClose}) {
     return (
         <View style={styles.container}>
             <View style={styles.content}>
                 <Text style={styles.title}>Senha Gerada</Text>
 
                 <Pressable style={styles.innerPassword}>
-                    <Text style={styles.text}>123123</Text>
+                    <Text style={styles.text}>{password}</Text>
                 </Pressable>
+
+                <View style={styles.buttonArea}>
+                    <TouchableOpacity style={styles.button} onPress={handleClose}>
+                        <Text style={styles.buttonText}>Voltar</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={[styles.button, styles.buttonSave]}>
+                        <Text style={styles.buttonSaveText}>Salvar senha</Text>
+                    </TouchableOpacity>
+                </View>
+
             </View>
         </View>
     )
@@ -24,7 +35,7 @@ const styles = StyleSheet.create({
     content: {
         backgroundColor: '#fff',
         width: '85%',
-        paddingTop: 24, 
+        paddingTop: 24,
         paddingBottom: 24,
         alignItems: 'center',
         justifyContent: 'center',
@@ -36,14 +47,37 @@ const styles = StyleSheet.create({
         color: '#000',
         marginBottom: 24
     },
-    innerPassword:{
-        backgroundColor:'#0e0e0e',
+    innerPassword: {
+        backgroundColor: '#0e0e0e',
         width: '90%',
         padding: 14,
         borderRadius: 8
     },
-    text:{
+    text: {
         color: '#fff',
         textAlign: 'center'
+    },
+    buttonArea: {
+        flexDirection: "row",
+        width: "90%",
+        marginTop: 8,
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    button: {
+        flex: 1,
+        alignItems: "center",
+        marginBottom: 14,
+        marginTop: 14,
+        padding: 8
+    },
+    buttonSave: {
+        backgroundColor: "#392de9",
+        borderRadius: 8
+    },
+    buttonSaveText: {
+        color: '#fff',
+        fontWeight: "bold"
     }
+
 })
